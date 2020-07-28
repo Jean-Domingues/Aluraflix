0e0eae624c13eea618503252adb3fab405726257
Jean Domingues <jeandomingues752@gmail.com>
2020-07-28 15:28:15 -0300

    Carrousel finalizado

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -5,9 +5,18 @@ import { Link } from 'react-router-dom';
 export default function CadastroCategoria() {
   return (
     <PageDefault>
-      <h1>Cadastro de Categorias</h1>
+      <h1>Cadastro de Categoria</h1>
 
-      <Link to="/">Ir para Home</Link>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>Cadastrar</button>
+      </form>
+
+      <Link to="/">Ir para home</Link>
     </PageDefault>
   );
 }
